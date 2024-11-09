761d3b3a26e3370909c7555c959937b9691bd03e
hugowu <anuo452@outlook.com>
2024-11-10 06:13:28 +0800

    add changetheme command

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,27 @@
-﻿namespace Bee.ViewModels;
+﻿using Avalonia;
+using Avalonia.Styling;
+using CommunityToolkit.Mvvm.Input;
+
+namespace Bee.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
 #pragma warning disable CA1822 // Mark members as static
     public string Greeting => "Welcome to Avalonia!";
 #pragma warning restore CA1822 // Mark members as static
+
+    /// <summary>
+    /// 改变主题方法
+    /// </summary>
+    [RelayCommand]
+    private void ChangeTheme()
+    {
+        if (Application.Current is null)
+        {
+            return;
+        }
+
+        Application.Current.RequestedThemeVariant = Application.Current.RequestedThemeVariant == ThemeVariant.Dark
+        ? ThemeVariant.Default : ThemeVariant.Dark;
+    }
 }
